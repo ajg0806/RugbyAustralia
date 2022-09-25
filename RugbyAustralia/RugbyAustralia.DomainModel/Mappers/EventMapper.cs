e3c08151102c6a09fe0b4bd4cf27c7e38cb8542e
ajg0806 <grabsch.adam@gmail.com>
2022-09-25 18:50:09 +1000

    Update EventMapper.cs
    Adjustments to eventMapper.cs

--- RugbyAustralia/RugbyAustralia.DomainModel/Mappers/EventMapper.cs
+++ RugbyAustralia/RugbyAustralia.DomainModel/Mappers/EventMapper.cs
@@ -11,45 +11,7 @@ namespace RugbyAustralia.DomainModel.Mappers
     {
         public static Event Map(EventDto dto)
         {
-            int? value = null;
-            if ((dto.value.All(char.IsDigit) || 
-                (dto.value[0] == '-' && 
-                dto.value[1..].All(char.IsDigit)))  
-                && dto.value.Length > 0)
-                value = Int32.Parse(dto.value);
-            else if (dto.value.Length > 0)
-            {
-                List<string> ops = new List<string>();
-                List<int> numbers = new List<int>();
-                int s = 0;
-                while(s < dto.value.Length)
-                {
-                    int l = 1;
-                    while (dto.value.Substring(s, l).All(char.IsDigit)
-                        && s + l < dto.value.Length)
-                        l++;
-                    string tmp = dto.value.Substring(s, l);
-                    if (char.IsDigit(tmp[0]) && !tmp.All(char.IsDigit))
-                        tmp = tmp[0..^1];
-                    if (tmp.All(char.IsDigit))
-                        numbers.Add(Int32.Parse(tmp));
-                    else
-                        ops.Add(tmp);
-                    s += tmp.Length;
-                }
-                int tmpNum = numbers.FirstOrDefault();
-                numbers.RemoveAt(0);
-                int i = 0;
-                numbers.ForEach(n => 
-                {
-                    if (ops.ElementAt(i) == "+")
-                        tmpNum += n;
-                    else if (ops.ElementAt(i) == "-")
-                        tmpNum -= n;
-                    i++;
-                });
-                value = tmpNum;
-            }    
+            int? value = GetValue(dto.value);
 
             return new Event
             {
@@ -71,6 +33,57 @@ namespace RugbyAustralia.DomainModel.Mappers
                 Qualifier5 = dto.qualifier5,
                 Value = value
             };
+        }
+        public static int? GetValue(string valString)
+        {
+            int? value = null;
+            if ((valString.All(char.IsDigit) ||
+               (valString[0] == '-' &&
+               valString[1..].All(char.IsDigit)))
+               && valString.Length > 0)
+                value = Int32.Parse(valString);
+            else if (valString.Length > 0)
+            {
+                List<string> ops = new List<string>();
+                List<int> numbers = new List<int>();
+                SeperateParts(ops, numbers, valString);
+                value = Math(ops, numbers);
+            }
+            return value;
+        }
+        public static int Math(List<string> ops, List<int> numbers)
+        {
+            int result = numbers.FirstOrDefault();
+            numbers.RemoveAt(0);
+            int i = 0;
+            numbers.ForEach(n =>
+            {
+                if (ops.ElementAt(i) == "+")
+                    result += n;
+                else if (ops.ElementAt(i) == "-")
+                    result -= n;
+                i++;
+            });
+            return result;
+        }
+        public static void SeperateParts(List<string> ops, List<int> numbers, string str)
+        {
+            int s = 0;
+            while (s < str.Length)
+            {
+                int l = 1;
+                while (str.Substring(s, l).All(char.IsDigit)
+                    && s + l < str.Length)
+                    l++;
+                string tmp = str.Substring(s, l);
+                if (char.IsDigit(tmp[0]) && !tmp.All(char.IsDigit))
+                    tmp = tmp[0..^1];
+                if (tmp.All(char.IsDigit))
+                    numbers.Add(Int32.Parse(tmp));
+                else
+                    ops.Add(tmp);
+                s += tmp.Length;
+            }
         }
     }
 }
